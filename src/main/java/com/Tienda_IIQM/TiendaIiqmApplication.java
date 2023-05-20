c9fdcb6d4975311cb79de8b0003215fc82d18e4f
Andres Morera <114629703+andims17@users.noreply.github.com>
2023-05-20 17:01:31 -0600

    Creacion del proyecto inicial

---
+++ src/main/java/com/Tienda_IIQM/TiendaIiqmApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_IIQM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIiqmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIiqmApplication.class, args);
+	}
+
+}
