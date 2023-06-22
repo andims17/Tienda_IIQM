2c3a71cde2dcf80622fa8707c1ceeb4db2d835cc
Andres Morera <114629703+andims17@users.noreply.github.com>
2023-06-21 21:30:35 -0600

    Subida clase 6

--- src/main/java/com/Tienda_IIQM/controler/CategoriaController.java
+++ src/main/java/com/Tienda_IIQM/controler/CategoriaController.java
@@ -6,13 +6,17 @@ package com.Tienda_IIQM.controler;
 
 import com.Tienda_IIQM.domain.Categoria;
 import com.Tienda_IIQM.service.CategoriaService;
+import com.Tienda_IIQM.service.impl.FirebaseStorageServiceImpl;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -25,6 +29,8 @@ public class CategoriaController {
     
     @Autowired
     CategoriaService categoriaService;
+    @Autowired
+    private FirebaseStorageServiceImpl firebaseStorageService;
     
     
     @GetMapping("/listado")
@@ -36,4 +42,39 @@ public class CategoriaController {
         return "/categoria/listado";
     }
     
+    @GetMapping("/nuevo")
+    public String categoriaNuevo(Categoria categoria) {
+        return "/categoria/modifica";
+    }
+
+    
+    @PostMapping("/guardar")
+    public String categoriaGuardar(Categoria categoria,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {        
+        if (!imagenFile.isEmpty()) {
+            categoriaService.save(categoria);
+            categoria.setRutaImagen(
+                    firebaseStorageService.cargaImagen(
+                            imagenFile, 
+                            "categoria", 
+                            categoria.getIdCategoria()));
+        }
+        categoriaService.save(categoria);
+        return "redirect:/categoria/listado";
+    }
+
+    @GetMapping("/eliminar/{idCategoria}")
+    public String categoriaEliminar(Categoria categoria) {
+        categoriaService.delete(categoria);
+        return "redirect:/categoria/listado";
+    }
+
+    @GetMapping("/modificar/{idCategoria}")
+    public String categoriaModificar(Categoria categoria, Model model) {
+        categoria = categoriaService.getCategoria(categoria);
+        model.addAttribute("categoria", categoria);
+        return "/categoria/modifica";
+    }
 }
+    
+
---
+++ src/main/java/com/Tienda_IIQM/service/FirebaseStorageService.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.Tienda_IIQM.service;
+
+/**
+ *
+ * @author Andres.M
+ */
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-daf53.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-daf53-firebase-adminsdk-7cgtn-9894c8d43a.json";
+}
