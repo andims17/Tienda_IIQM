3ed5005daf8cd22cae77c710a49b58032d619490
Andres Morera <114629703+andims17@users.noreply.github.com>
2023-07-31 20:00:17 -0600

    Clase numero 11

---
+++ src/main/java/com/Tienda_IIQM/controler/UsuarioController.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Controller.java to edit this template
+ */
+package com.Tienda_IIQM.controler;
+
+import com.Tienda_IIQM.domain.Usuario;
+import com.Tienda_IIQM.service.FirebaseStorageService;
+import com.Tienda_IIQM.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author Andres.M
+ */
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        var usuarios = usuarioService.getUsuarios();
+        model.addAttribute("usuarios", usuarios);
+        model.addAttribute("totalUsuarios", usuarios.size());
+        return "/usuario/listado";
+    }
+
+    @GetMapping("/nuevo")
+    public String usuarioNuevo(Usuario usuario) {
+        return "/usuario/modifica";
+    }
+
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
+    
+            
+
+    @PostMapping("/guardar")
+    public String usuarioGuardar(Usuario usuario,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {
+		usuario.setPassword(new BCryptPasswordEncoder().encode(usuario.getPassword()));
+        if (!imagenFile.isEmpty()) {
+            usuarioService.save(usuario,false);
+            usuario.setRutaImagen(
+                    firebaseStorageService.cargaImagen(
+                            imagenFile,
+                            "usuario",
+                            usuario.getIdUsuario()));
+        }
+        usuarioService.save(usuario,true);
+        return "redirect:/usuario/listado";
+    }
+
+    @GetMapping("/eliminar/{idUsuario}")
+    public String usuarioEliminar(Usuario usuario) {
+        usuarioService.delete(usuario);
+        return "redirect:/usuario/listado";
+    }
+
+    @GetMapping("/modificar/{idUsuario}")
+    public String usuarioModificar(Usuario usuario, Model model) {
+        usuario = usuarioService.getUsuario(usuario);
+        model.addAttribute("usuario", usuario);
+        return "/usuario/modifica";
+    }
+}
---
+++ src/main/java/com/Tienda_IIQM/dao/RolDao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.Tienda_IIQM.dao;
+
+/**
+ *
+ * @author Andres.M
+ */
+
+import com.Tienda_IIQM.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+    
+    
+}
