e5f258bca9ef40e5c5bc5c86dff11e857456b28b
Andres Morera <114629703+andims17@users.noreply.github.com>
2023-08-09 20:43:13 -0600

    Semana 13 carrito compras

--- src/main/java/com/Tienda_IIQM/controler/IndexController.java
+++ src/main/java/com/Tienda_IIQM/controler/IndexController.java
@@ -4,7 +4,9 @@
  */
 package com.Tienda_IIQM.controler;
 
+import com.Tienda_IIQM.service.ProductoService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     
+    @Autowired
+    ProductoService productoService;
+    
     @RequestMapping("/")
-    public String page(Model model , HttpSession session) {
-        String imagen = (String)session.getAttribute("usuarioImagen");
-        model.addAttribute("avatar",imagen);
+    public String page(Model model , HttpSession session) {        
+        var productos = productoService.getProductos(true);
+        model.addAttribute("productos",productos); 
         return "index";
     }
     
---
+++ src/main/java/com/Tienda_IIQM/domain/Venta.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Tienda_IIQM.domain;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ *
+ * @author Andres.M
+ */
+@Data
+@Entity
+@Table(name="venta")
+public class Venta implements Serializable {    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_venta")
+    private Long idVenta;
+    private Long idFactura;
+    private Long idProducto;    
+    private double precio;
+    private int cantidad;    
+    
+    public Venta() {
+    }
+
+    public Venta(Long idFactura, Long idProducto, double precio, int cantidad) {
+        this.idFactura = idFactura;
+        this.idProducto = idProducto;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+}
